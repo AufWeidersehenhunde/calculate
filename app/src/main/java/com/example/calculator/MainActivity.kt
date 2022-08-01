8b82ac2914270f9b9950c2ca267233aedcaed0f6
Alexey <schneiderff37@gmail.com>
2022-08-01 23:21:52 +0500

    calcul2

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -8,16 +8,20 @@ import android.widget.Toast
 import com.example.calculator.databinding.ActivityMainBinding
 import net.objecthunter.exp4j.ExpressionBuilder
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var operation:String
+  class MainActivity : AppCompatActivity() {
     private lateinit var firstnum:String
     private lateinit var secondnum:String
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var io:String
+    private lateinit var oi:String
+
+
+      private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val chisla = arrayOf(0,1,2,3,4,5,6,7,8,9)
+
+
 
 fun firstnum() {
 
@@ -100,77 +104,80 @@ firstnum()
             else
                 firstnum()
         }
-        fun operation() {
+
+
+
+
+
             binding.bplus.setOnClickListener {
-                binding.pt3.setText("+")
+
+                if (binding.pt1.text.toString() == ""){
+                    Toast.makeText(applicationContext, "Lox", Toast.LENGTH_SHORT).show()
+
+                }
+                if (binding.pt2.text.toString() == ""){
+                    Toast.makeText(applicationContext, "Lox", Toast.LENGTH_SHORT).show()
+                   
+                }
+                val io = binding.pt1.text.toString()
+                val oi = binding.pt2.text.toString()
+                val cio = io.toDouble()+oi.toDouble()
+                binding.pt1.setText("$cio")
+                binding.pt2.setText("")
+
+
             }
             binding.bminus.setOnClickListener {
-                binding.pt3.setText("-")
+                val io = binding.pt1.text.toString()
+                val oi = binding.pt2.text.toString()
+                if (binding.pt1.text.toString() == ""){
+                    Toast.makeText(applicationContext, "Lox", Toast.LENGTH_SHORT).show()
+                }
+                if (binding.pt2.text.toString() == ""){
+                    Toast.makeText(applicationContext, "Lox", Toast.LENGTH_SHORT).show()
+                }
+                val lio = io.toDouble()-oi.toDouble()
+                binding.pt1.setText("$lio")
+                binding.pt2.setText("")
             }
             binding.bdevide.setOnClickListener {
-                binding.pt3.setText("/")
+                val io = binding.pt1.text.toString()
+                val oi = binding.pt2.text.toString()
+                if (binding.pt1.text.toString() == ""){
+                    Toast.makeText(applicationContext, "Lox", Toast.LENGTH_SHORT).show()
+                }
+                if (binding.pt2.text.toString() == ""){
+                    Toast.makeText(applicationContext, "Lox", Toast.LENGTH_SHORT).show()
+                }
+
+                val kio = io.toDouble()/oi.toDouble()
+                binding.pt1.setText("$kio")
+                binding.pt2.setText("")
+
             }
             binding.bumnozh.setOnClickListener {
-                binding.pt3.setText("*")
+                val io = binding.pt1.text.toString()
+                val oi = binding.pt2.text.toString()
+                if (binding.pt1.text.toString() == ""){
+                    Toast.makeText(applicationContext, "Lox", Toast.LENGTH_SHORT).show()
+                }
+                if (binding.pt2.text.toString() == ""){
+                    Toast.makeText(applicationContext, "Lox", Toast.LENGTH_SHORT).show()
+                }
+                val gio = io.toDouble()*oi.toDouble()
+                binding.pt1.setText("$gio")
+                binding.pt2.setText("")
             }
             binding.bdelete.setOnClickListener {
                 binding.pt1.setText("")
                 binding.pt2.setText("")
-                binding.pt3.setText("")
-            }
-
 
             }
-
-            binding.bresult.setOnClickListener {
-                if (binding.pt1.text.toString() == "" ) {
-                    Toast.makeText(applicationContext, "Ты, блять, может первое число введешь?", Toast.LENGTH_SHORT).show()
-                    onPause()}
-
-                if (binding.pt2.text.toString() == "" ) {
-                    Toast.makeText(applicationContext, "Ты, блять, может второе число введешь?", Toast.LENGTH_SHORT).show()
-                    onPause()}
-
-                if (binding.pt3.text.toString() == "" ) {
-                    onPause()
-                    Toast.makeText(applicationContext, "Ты, блять, может хочешь посчитать?", Toast.LENGTH_LONG).show()
-                }
-                val io = binding.pt1.text.toString()
-                val oi = binding.pt2.text.toString()
-                val te = binding.pt3.text.toString()
-
-                val cio = io.toDouble()+oi.toDouble()
-                val lio = io.toDouble()-oi.toDouble()
-                val gio = io.toDouble()*oi.toDouble()
-                val kio = io.toDouble()/oi.toDouble()
-                    if (te == "+"){
-                        binding.pt1.setText("$cio")
-                        binding.pt2.setText("")
-                        binding.pt3.setText("")
-                    }
-                if (te == "-"){
-                    binding.pt1.setText("$lio")
-                    binding.pt2.setText("")
-                    binding.pt3.setText("")
-                }
-                if (te == "*") {
-                    binding.pt1.setText("$gio")
-                    binding.pt2.setText("")
-                    binding.pt3.setText("")
-                }
-                if (te == "/") {
-                    binding.pt1.setText("$kio")
-                    binding.pt2.setText("")
-                    binding.pt3.setText("")
-                }
-                if (binding.pt1.text.toString() == "1488"){
-                    Toast.makeText(applicationContext, "Нацист блять", Toast.LENGTH_SHORT).show()
-                }
 
 
 
             }
-        operation()
+
         }
 
 
@@ -180,7 +187,7 @@ firstnum()
 
 
 
-    }
+
 
 
 
